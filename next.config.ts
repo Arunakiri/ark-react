5704bd2f8346b09c04645c62ea5eda0463fee963
arunakiri <arunakiri.v@gmail.com>
2026-04-10 01:06:14 +0530

    Update basePath to match GitHub repo name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-app",
+  basePath: "/ark-react",
   images: {
     unoptimized: true,
   },
